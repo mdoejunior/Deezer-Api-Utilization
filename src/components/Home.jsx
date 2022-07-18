4b352c2d799d694b5c8dea38451aa462c9be37bf
mdoejunior <issamdoe2019@gmail.com>
2022-07-18 03:11:42 +0300

    Deezer Changes

---
+++ src/components/Home.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import image from "./../assets/undraw_happy_music_g6wc.svg";
+// import github from "./../assets/mark-github.svg"
+
+export default function Home() {
+  return (
+    <div className="home-container">
+      <div className="home-left">
+        <div className="home-title">DeezerLike Player</div>
+        <div className="home-desc">
+          A responsive web-based music streaming app built with React, Redux for state management and
+          Deezer API
+        </div>
+        <Link to="/player/albums">
+          <button className="custom-button">Go to Player</button>
+        </Link>
+      </div>
+      <div className="home-right">
+        <img src={image} alt="music vector" />
+      </div>
+    </div>
+  );
+}
